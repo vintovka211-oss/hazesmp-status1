34657ed4f9864989cf6b700e36c8031656abbb02
vintovka211-oss <vintovka211@gmail.com>
2026-05-20 17:32:17 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+ADMINS = [8493522297, 5449297683]
+
+# ЮKassa
+YKASSA_SHOP_ID = os.getenv("YKASSA_SHOP_ID")      # ID магазина
+YKASSA_SECRET_KEY = os.getenv("YKASSA_SECRET_KEY")  # Секретный ключ
