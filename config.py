9fe71849c47f0e0cf8296c918a1260bf50a4cc75
vintovka211-oss <vintovka211@gmail.com>
2026-05-20 19:24:36 +0300

    Update config.py

--- config.py
+++ config.py
@@ -6,3 +6,5 @@ ADMINS = [8493522297, 5449297683]
 # ЮKassa
 YKASSA_SHOP_ID = os.getenv("YKASSA_SHOP_ID")      # ID магазина
 YKASSA_SECRET_KEY = os.getenv("YKASSA_SECRET_KEY")  # Секретный ключ
+
+YUMONEY_WALLET = "410011234567890"  # Ваш номер кошелька (10-11 цифр)
